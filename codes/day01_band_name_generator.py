af1f3eedab0ae9ffeeeddd479c6b6d8b841f2668
David <brightenway@outlook.com>
2025-08-27 22:15:43 +0900

    Day 1 Project

---
+++ codes/day01_band_name_generator.py
@@ -0,0 +1,26 @@
+# Tip) command + / will make the line into comment.
+# print function
+print("Hello world")
+print("Hello world!\nHello world!")
+print("Hello" + "David") #Note that there's no gap between two words.
+print("Hello" + " " + "David")
+
+# input function
+print("Hello" + " " + input("What is your name?: ") + "!")
+
+# variable
+name = input("What is your name?")
+print(name)
+
+# Return the length of the string
+print(len(name))
+username = input("What is your name?")
+print(username)
+print(len(username))
+
+# Day1 Project: Band Name Generator
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
+
