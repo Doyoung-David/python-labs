addb4103c97d2bd9fd6989aa23b31a261b3c74ae
David <brightenway@outlook.com>
2025-09-01 22:25:52 +0900

    Day 7: hangman project with loops and conditionals

---
+++ codes/day07_hang_man.py
@@ -0,0 +1,104 @@
+# Hang_man
+import random
+stages = [
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
+lives = 6
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+placeholder = ""
+for k in chosen_word:
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    user_word = input("Guess the letter.: ").lower()
+    display = ""
+    for i in chosen_word:
+        if user_word == i:
+            print("Right")
+            display += i
+            correct_letters.append(i) 
+        elif i in correct_letters:
+            display += i
+        else:
+            print("Wrong")
+            display += "_"
+    print(display)
+    if i not in chosen_word:
+        lives -= 1
+        if lives == 0:
+         game_over = True
+         print("You lose.") 
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+    print(stages[lives])    
